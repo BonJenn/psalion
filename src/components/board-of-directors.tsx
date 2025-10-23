f2bcb6e69c4efedb88896b6ba04f16a3602505b0
Jonathan Benn <bonjennprojects@gmail.com>
2025-10-23 14:48:39 -0700

    Board of Directors: fix section height to prevent layout shift; scrollable description pane

--- src/components/board-of-directors.tsx
+++ src/components/board-of-directors.tsx
@@ -54,7 +54,7 @@ export default function BoardOfDirectors() {
           </div>
         </motion.div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12 items-start min-h-[520px] md:min-h-[560px] lg:min-h-[600px]">
           {/* Directors List */}
           <motion.div
             className="space-y-4"
@@ -96,7 +96,7 @@ export default function BoardOfDirectors() {
 
           {/* Director Description */}
           <motion.div
-            className="lg:pl-8 mt-8 lg:mt-0"
+            className="lg:pl-8 mt-8 lg:mt-0 h-[360px] sm:h-[380px] md:h-[420px] lg:h-[460px] overflow-y-auto pr-2"
             key={selectedDirector}
             initial={{ opacity: 0, x: 20 }}
             animate={{ opacity: 1, x: 0 }}
